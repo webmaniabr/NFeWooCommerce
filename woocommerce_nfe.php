eca51ab24c2d080aade36c7b1bce3e784a96c0f2
Equipe WebmaniaBR® <suporte@webmaniabr.com>
2016-06-22 22:25:33 -0300

    v 1.0.4
    Correção de incompatibilidade das chaves em algumas versões do PHP onde
    ocorria erro fatal ao ativar o plugin.

--- woocommerce_nfe.php
+++ woocommerce_nfe.php
@@ -5,7 +5,7 @@
  * Description: Módulo de emissão de Nota Fiscal Eletrônica para WooCommerce através da REST API da WebmaniaBR®.
  * Author: WebmaniaBR
  * Author URI: http://webmaniabr.com
- * Version: 1.0.3
+ * Version: 1.0.4
  * Copyright: © 2009-2016 WebmaniaBR.
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
