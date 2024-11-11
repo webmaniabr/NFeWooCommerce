b5dd4f368ed8f22152ae986ead449b25fc97aa97
Webmania® <thyago@webmaniabr.com>
2024-11-11 16:16:04 -0300

    v3.4.0.2

--- init-class.php
+++ init-class.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WooCommerceNFe {
 
 	public $domain = 'WooCommerceNFe';
-	public $version = '3.4.0.1';
+	public $version = '3.4.0.2';
 	protected static $_instance = NULL;
 
 	public static function instance() {
